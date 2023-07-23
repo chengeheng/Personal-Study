a038810ca189f961327ab2e5d95af4d1888c2486
chengeheng <465819676@qq.com>
2023-07-23 10:36:41 +0800

    :sparkles: feat(electron): electron生命周期实验

--- electron/main.js
+++ electron/main.js
@@ -14,8 +14,17 @@ const createWindow = () => {
 
   mainWin.loadFile("index.html");
 
+  mainWin.webContents.on("did-finish-load", () => {
+    console.log("03 -> did finish load");
+  });
+
+  mainWin.webContents.on("dom-ready", () => {
+    console.log("02 -> dom ready");
+  });
+
   mainWin.on("close", () => {
-    console.log("main window closed~~~");
+    console.log("04 -> main window closed");
+    mainWin = null; // 清空引用关系
   });
 };
 
@@ -23,6 +32,7 @@ const createWindow = () => {
  * 当app启动之后执行窗口创建等操作
  */
 app.whenReady().then(() => {
+  console.log("01 -> app ready");
   createWindow();
 
   app.on("activate", () => {
@@ -31,6 +41,15 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-  console.log("all window closed");
+  console.log("05 -> all window closed");
   if (process.platform !== "darwin") app.quit();
+});
+app.on("before-quit", () => {
+  console.log("06 -> before quit");
+});
+app.on("will-quit", () => {
+  console.log("07 -> will quit");
+});
+app.on("quit", () => {
+  console.log("08 -> quit");
 });
