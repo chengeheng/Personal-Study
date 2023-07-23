45b2f799e217a228fe42e9149fd9f875405c91c8
chengeheng <465819676@qq.com>
2023-07-23 10:50:10 +0800

    :sparkles: feat(electron): 设置应用窗口大小

--- .cz-config.js
+++ .cz-config.js
@@ -49,7 +49,7 @@ module.exports = {
       name: "👷  ci:       CI related changes",
     },
   ],
-  scopes: ["electron"],
+  scopes: [{ name: "electron" }],
   messages: {
     type: "请选择提交类型(必填)",
     customScope: "请输入文件修改范围(可选)",
--- electron/main.js
+++ electron/main.js
@@ -7,12 +7,24 @@ const path = require("path");
  */
 const createWindow = () => {
   const mainWin = new BrowserWindow({
+    x: 100, // 窗口显示的位置，相对屏幕左上角的横坐标
+    y: 100, // 窗口显示的位置，相对屏幕左上角的纵坐标
+    show: false, // 默认情况下创建一个窗口对象后就会显示，设置为false就不会显示了
     width: 800,
     height: 600,
+    maxWidth: 1000, // 窗口最大宽度
+    maxHeight: 800, // 窗口最大高度
+    minWidth: 400, // 窗口最小宽度
+    minHeight: 400, // 窗口最小高度
+    resizable: false, // 控制窗口大小是否可修改，即是否可缩放
     // webPreferences: {}
   });
 
   mainWin.loadFile("index.html");
+
+  mainWin.on("ready-to-show", () => {
+    mainWin.show();
+  });
 
   mainWin.webContents.on("did-finish-load", () => {
     console.log("03 -> did finish load");
