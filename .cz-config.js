3128b1253867459dea3e396475a48bffdbb905e0
chengeheng <465819676@qq.com>
2023-07-29 02:27:32 +0800

    :rocket: chore(shell): 新增shell模块

--- .cz-config.js
+++ .cz-config.js
@@ -49,7 +49,7 @@ module.exports = {
       name: "👷  ci:       CI related changes",
     },
   ],
-  scopes: [{ name: "electron" }],
+  scopes: [{ name: "electron" }, { name: "shell" }],
   messages: {
     type: "请选择提交类型(必填)",
     customScope: "请输入文件修改范围(可选)",
